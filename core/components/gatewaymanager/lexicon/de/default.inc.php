535c87d8c9732d9624e0d97edf3568d6d523f44c
Anselm Hannemann <anselm@novolo.de>
2012-07-06 22:27:45 +0200

    added German language
    Signed-off-by: Anselm Hannemann <anselm@novolo.de>

---
+++ core/components/gatewaymanager/lexicon/de/default.inc.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The default German language topics
+ * 
+ * @package gatewaymanager
+ * @subpackage lexicon
+ * @language de
+ * @author Anselm Hannemann at anselm-hannemann.com <info@anselm-hannemann.com>
+ */
+
+$_lang['gatewaymanager'] = "Gateway Manager";
+$_lang['gatewaymanager.desc'] = "Verwalten Sie Domains und entscheiden Sie welche Seite zu welcher Domain gehört.";
+$_lang['gatewaymanager.manage'] = "Verwalten Sie Ihre Gateway Domains";
+$_lang['gatewaymanager.gateways'] = "Gateways";
+$_lang['gatewaymanager.gateways.desc'] = "Unten finden Sie eine Liste an Domains und dazugehörigen Kontexten (und/oder abweichende Startseiten), die Sie angelegt haben. Sie können einfach eine neue Gateway Domain hinzufügen oder bestehende bearbeiten.";
+$_lang['gatewaymanager.search'] = "Suchen&hellip;";
+
+$_lang['gatewaymanager.create'] = "Neuen Gateway erstellen";
+$_lang['gatewaymanager.domain'] = "Domain";
+$_lang['gatewaymanager.context'] = "Kontext";
+$_lang['gatewaymanager.context.createsettings'] = "Notwendige Kontexteinstellungen erstellen";
+$_lang['gatewaymanager.context.createsettings.desc'] = "Dies erstellt die zwei benötigten Kontexteinstellungen \"site_url\" und \"http_host\" für den ausgewählten Kontext (falls noch nicht erstellt!).";
+$_lang['gatewaymanager.startpage'] = "Startseite";
+$_lang['gatewaymanager.startpage.id'] = "ID";
+$_lang['gatewaymanager.startpage.default'] = "Standardseite";
+$_lang['gatewaymanager.active'] = "Aktiv";
+$_lang['gatewaymanager.remove'] = "Gateway entfernen";
+$_lang['gatewaymanager.remove_confirm'] = "Sind Sie sich sicher, dass Sie diesen Gateway entfernen möchten?";
+
+$_lang['gatewaymanager.error.domain_ns'] = "Domainname nicht spezifiziert. Bitte geben Sie einen Domainnamen an!";
+$_lang['gatewaymanager.error.domain_ae'] = "Domain ist bereits angelegt. Bitte verwenden Sie einen anderen Domainnamen!";
+$_lang['gatewaymanager.error.save'] = "Speichern des Gateway fehlgeschlagen. Laden Sie die Seite neu und versuchen Sie es erneut!";
+$_lang['gatewaymanager.error.id_ns'] = "Domain ID nicht angegeben. Bitte versuchen Sie es erneut!";
+$_lang['gatewaymanager.error.nf'] = "Domain nicht gefunden. Laden Sie die Seite neu und versuchen Sie es erneut!";
+$_lang['gatewaymanager.error.remove'] = "Domain konnte nicht entfernt werden. Laden Sie die Seite neu und versuchen Sie es erneut!";
+
+?>
